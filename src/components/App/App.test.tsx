e5c7398d1ad90cfc02276ced4b77db4def81fc8f
SelimWebDev <selim.elmahi@gmail.com>
2022-02-09 10:12:25 +0100

    fix(map): ajout d'un composant Provider au test unitaire du composant Map

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { store } from '../../store/store';
+import { Provider } from 'react-redux';
 
 test('renders learn react link', async () => {
-  render(<App />);
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
   const AppElement = await screen.findByTestId('App');
   expect(AppElement).toBeInTheDocument();
 });
